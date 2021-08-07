104907613e22efa37a33170befe147af39f30c8d
ayushrajput158 <87794509+ayushrajput158@users.noreply.github.com>
2021-08-07 13:56:53 +0530

    input and output

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
-console.log(txtInput);
+var outputDiv = document.querySelector("#output");
+
+
 function clickHandler(){
-console.log("clicked !");
-console.log("input", txtInput.value);
+outputDiv.innerText = "Ajsjsjsjsj " + txtInput.value;
 };
 btnTranslate.addEventListener("click", clickHandler) 
  
