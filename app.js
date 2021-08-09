b6891ccf291e6dd057787c0a9f35da0b170f24ab
ayushrajput158 <87794509+ayushrajput158@users.noreply.github.com>
2021-08-10 01:17:35 +0530

    done

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var outputDiv = document.querySelector("#output");
 var serverURL = "https://api.funtranslations.com/translate/minion.json"
 
  function getTranslationURL(input) {
-     return serverURL + "?" + "text=" + input
+     return serverURL + "?text=" + input
  }
  function errorHandler(error){
      console.log("error occured", error);
@@ -14,10 +14,11 @@ function clickHandler() {
     //taking input 
     var inputText = txtInput.value;
   // calling server for processing
+
   fetch(getTranslationURL(inputText))
   .then(response => response.json())
   .then(json=> {
-      var TranslatedText = json.contents.translated ;
+      let translatedText = json.contents.translated ;
       outputDiv.innerText =  translatedText;
   })
   .catch(errorHandler)
